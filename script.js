a407a7fe0d299129f9ef120004b3049682bdcd9c
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-02-25 09:01:51 +0530

    Update JS
    Update JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
+    statusTxt = form.querySelector(".button-area span");
 
-form.onsubmit = (e)=>{
+form.onsubmit = (e) => {
     e.preventDefault(); //preventing form from auto sumbiting
 }
