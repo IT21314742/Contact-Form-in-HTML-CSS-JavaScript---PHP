fc373bbb9274ac3f79d5c650dd0b8368a7f5aab6
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-02-25 09:06:14 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const form = document.querySelector("form"),
 form.onsubmit = (e) => {
     e.preventDefault(); //preventing form from auto sumbitting
     statusTxt.style.color = "#0D6EFD";
-    statusTxt.style.display = "block"
+    statusTxt.style.display = "block";
 }
