aa151bdf74049338918de15e08712172e5afb9a0
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-02-28 21:48:47 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ form.onsubmit = (e) => {
         let response = xhr.response; //storing ajax response in a response variable
 
         //if response is an error like below one of them status color switch to red if not form will reset
-        if(response,indexeOf("Email and message is required!") != -1 || response.indexeOf("Enter a valid email Email address") ||)
+        if(response,indexeOf("Email and message is required!") != -1 || response.indexeOf("Enter a valid email Email address") || response)
     }
 }
