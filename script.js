b03110ffdce6f50fad5441c6093de6b6d2ae1cfa
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-03-01 23:43:54 +0530

    Add JS
    add js

--- script.js
+++ script.js
@@ -26,5 +26,5 @@ form.onsubmit = (e) => {
     }
 }
 
-let formData = new FormData(form); //Creating new FormData object. this object is used to
+let formData = new FormData(form); //Creating new FormData object. this object is used to send
 }
