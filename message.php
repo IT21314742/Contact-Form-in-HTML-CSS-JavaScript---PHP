478ed52761c60c4dec5b8bb848d90c36c65075c4
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-03-03 11:14:45 +0530

    Update PHP
    add php

--- message.php
+++ message.php
@@ -5,7 +5,7 @@ $phone = $_POST['phone'];
 $website = $_POST['website'];
 $message = $_POST['message'];
 
-if(!empty($email) && !empty ($message)){  //if email and message field is
+if(!empty($email) && !empty ($message)){  //if email and message field is not
 
 }
 ?>
