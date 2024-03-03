c81fa7fa6a8161b8d0c30176c2918bb05aa424bd
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-03-03 12:07:05 +0530

    Update PHP
    add php

--- message.php
+++ message.php
@@ -8,7 +8,7 @@ $message = $_POST['message'];
 if(!empty($email) && !empty ($message)){  //if email and message field is not empty
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){ //validate user entered Email
     $receiver = "adhikurera@gmail.com"; //validate receivered email address
-    $subject =  "form: $"    
+    $subject =  "form: $name"    
     }
 }
 ?>
