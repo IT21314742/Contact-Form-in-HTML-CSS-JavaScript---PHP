cfb5e429d69dba8ffcb99cbb6f910c269dd40554
IT21314742 <it21314742@my.sliit.lk>
2024-03-06 22:27:28 +0530

    Update PHP
    Update PHP

--- message.php
+++ message.php
@@ -19,6 +19,6 @@ if(!empty($email) && !empty ($message)){  //if email and message field is not em
     }
 
     }else{
-        echo "Enter a valid emai"
+        echo "Enter a valid email"
     }
 }
