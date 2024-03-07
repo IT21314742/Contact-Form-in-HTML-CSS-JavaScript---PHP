19dde9c278837e68c7ab88b79b81015f77b6acca
IT21314742 <it21314742@my.sliit.lk>
2024-03-07 12:44:06 +0530

    Update PHP
    add php

--- message.php
+++ message.php
@@ -22,5 +22,5 @@ if(!empty($email) && !empty ($message)){  //if email and message field is not em
         echo "Enter a valid email address!";
     }
 }else{
-    echo "Email and message is "
+    echo "Email and message is required!"
 }
